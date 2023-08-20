e5e86880a88356de4521e11a792efb72ce6e73cf
Diandson C. Ramos <diandsonc@gmail.com>
2023-08-20 11:52:54 +0100

    feat: implement UseCipherKey extension method (#19)
    - Added the UseCipherKey extension method to IApplicationBuilder
    - Enables easy integration of CipherKey authentication into application services
    - Simplifies the setup process for CipherKey authentication configuration
    
    Closes #18

---
+++ src/CipherKey/Extensions/CipherKeyBuildExtensions.cs
@@ -0,0 +1,21 @@
+namespace Microsoft.AspNetCore.Builder
+{
+    /// <summary>
+    /// Provides extension methods to add authentication and authorization capabilities to an HTTP application pipeline.
+    /// </summary>
+    public static class CipherKeyBuildExtensions
+    {
+        /// <summary>
+        /// Adds authentication and authorization middleware to the application pipeline.
+        /// </summary>
+        /// <param name="app">The application builder.</param>
+        /// <returns>The instance of <see cref="IApplicationBuilder"/>.</returns>
+        public static IApplicationBuilder UseCipherKey(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            return app;
+        }
+    }
+}
